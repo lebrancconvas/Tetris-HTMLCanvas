8caf8da761a83de73eff17d2355f24c8b19d4f7d
lebrancconvas <lebrancconvas@gmail.com>
2023-09-04 11:24:47 +0700

    ✨ feat: update tetrominoes fall logic

--- src/main.ts
+++ src/main.ts
@@ -9,20 +9,30 @@ canvas.style.backgroundColor = config.backgroundColor;
 // Code.
 
 // Title.
-// ctx.font = '48px Arial';
-// ctx.fillStyle = 'black';
-// ctx.textAlign = 'center';
-// ctx.fillText('Tetris', canvas.width / 2, 100);
+ctx.font = '23px Arial';
+ctx.fillStyle = 'Black';
+ctx.textAlign = 'center';
+ctx.fillText('Tetris', canvas.width / 2, 30);
 
 // Board.
 const boardWidth = 400;
 const boardHeight = 800;
 const board = new Board((canvas.width - boardWidth) / 2, (canvas.height - boardHeight) / 2, boardWidth, boardHeight);
-board.draw(ctx);
 
 // Tetrominos.
 const i = new I((canvas.width - boardWidth) / 2, (canvas.height - boardHeight) / 2, 'Green');
+const o = new O((canvas.width - boardWidth) / 2, (canvas.height - boardHeight) / 2, 'Blue');
+
+board.draw(ctx);
 i.draw(ctx);
 
-const o = new O((canvas.width - boardWidth) / 2, (canvas.height - boardHeight) / 2, 'Blue');
-o.draw(ctx);
+setInterval(() => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'Black';
+  ctx.fillText('Tetris', canvas.width / 2, 30);
+  board.draw(ctx);
+  if(i.y < (canvas.height - boardHeight) / 2 + boardHeight - 40) {
+    i.y += 40;
+  }
+  i.draw(ctx);
+}, 1000);
