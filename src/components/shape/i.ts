794c1f236098f78592d7bf2c39becf2b2aff92d6
lebrancconvas <lebrancconvas@gmail.com>
2023-09-04 10:26:15 +0700

    ✨ feat: update tetromino "I"

---
+++ src/components/shape/i.ts
@@ -0,0 +1,30 @@
+import { Rect } from "./rect";
+
+export class I {
+  x: number;
+  y: number;
+  color: string;
+
+  constructor(x: number, y: number, color: string) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
+
+  draw(c: CanvasRenderingContext2D) {
+    const rectWidth = 50;
+    const rectHeight = 50;
+
+    const rect1 = new Rect(this.x, this.y, rectWidth, rectHeight, this.color);
+    rect1.draw(c);
+
+    const rect2 = new Rect(this.x + rectWidth, this.y, rectWidth, rectHeight, this.color);
+    rect2.draw(c);
+
+    const rect3 = new Rect(this.x + rectWidth * 2, this.y, rectWidth, rectHeight, this.color);
+    rect3.draw(c);
+
+    const rect4 = new Rect(this.x + rectWidth * 3, this.y, rectWidth, rectHeight, this.color);
+    rect4.draw(c);
+  }
+}; 
